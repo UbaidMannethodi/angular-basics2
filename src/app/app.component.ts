e0886845738d473d5f2b1a1aa93a7d9168988246
ubaidmannethodi <ubaidmannethodi@gmail.com>
2021-09-20 19:24:16 +0530

    if and for loop functions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  show =true;
 
+color = "red" ;
+
+
+user =[
+  {name:"unbaid", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]},
+  {name:"unbagftwsgid", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]},
+  {name:"dgs", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]},
+  {name:"fhygadfh", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]}
+]
 
 }
