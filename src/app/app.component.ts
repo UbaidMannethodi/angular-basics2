3f57cac1e81668969e1075f4e428412020b64d4d
ubaid mannethodi <ubaidmlpm4@gmail.com>
2021-09-20 23:16:55 +0530

    added bootstrap library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
-  show =true;
+  show = true;
+  color = 'red';
 
-color = "red" ;
-
-
-user =[
-  {name:"unbaid", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]},
-  {name:"unbagftwsgid", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]},
-  {name:"dgs", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]},
-  {name:"fhygadfh", place: "pullara", accounts:['gsd','gsd','gsd','gsd',]}
-]
-
+  user = [
+    {
+      name: 'unbaid',
+      place: 'pullara',
+      accounts: ['gsd', 'gsd', 'gsd', 'gsd'],
+    },
+    {
+      name: 'unbagftwsgid',
+      place: 'pullara',
+      accounts: ['gsd', 'gsd', 'gsd', 'gsd'],
+    },
+    { name: 'dgs', place: 'pullara', accounts: ['gsd', 'gsd', 'gsd', 'gsd'] },
+    {
+      name: 'fhygadfh',
+      place: 'pullara',
+      accounts: ['gsd', 'gsd', 'gsd', 'gsd'],
+    },
+  ];
 }
